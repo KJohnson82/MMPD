8414a61520222a67d2f10a056f2b03036de596f0
KJohnson82 <rojohnson@gmail.com>
2025-08-21 11:03:53 -0500

    Deleted old and unused backups

--- MMPD.Shared_Backup_2025.06.27_01.27.04/Services/IFormFactor.cs
+++
@@ -1,8 +0,0 @@
-namespace MMPD.Shared.Services
-{
-    public interface IFormFactor
-    {
-        public string GetFormFactor();
-        public string GetPlatform();
-    }
-}
--- MMPD.Shared_Backup_2025.06.27_01.27.04/wwwroot/js/app.js
+++
@@ -1,5 +0,0 @@
-﻿
-
-function goBack() {
-    window.history.back();
-}
